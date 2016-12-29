d5f37ad370351d7bc9f00d6436bbaa668ee926ce
lcc19941214 <245987239@qq.com>
2016-12-30 00:02:10 +0800

    feat: promised mongoose
    Change-Id: I37511495fb99af78b92e73980f3a06fca425710e

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -4,6 +4,10 @@ const mongoDB = MODE === 'production' ? 'zhihu_crawl' : 'test';
 console.log(`mongoDB: ${mongoDB}`);
 
 const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+
+// promised mongoose
+mongoose.Promise = bluebird;
 
 mongoose.connect(`mongodb://localhost/${mongoDB}`);
 const db = mongoose.connection;
